963c0d56466c47358d93a21ea8235a27fe32f02c
rzatalibov555 <sethubpro@gmail.com>
2025-03-29 21:08:56 +0400

    Shell 2

--- product/views.py
+++ product/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.db.models import Q
 
 def index(request):
     return render(request, "product/index.html")
@@ -11,3 +11,18 @@ def product_detail(request):
     return render(request, "product/shop-single.html")
 
 
+# & - AND // VE
+# | - OR  // VE YA
+
+
+# Product.objects.filter(
+# +      Q(author_id=1)
+# AND    &
+# +      Q(author__name="Aytac")
+#)
+
+# Product.objects.filter(
+# -      Q(author_id=1)
+# OR     |
+# +      Q(author__name="Aytac")
+#)
